e4df7af203deb5543cefd564fbda0afe1ab28ddb
Hyeongwoo Kim <snake1754@gmail.com>
2019-03-08 21:54:04 +0900

    Solve pg-42588

---
+++ programmers/level2/42588/solution.py
@@ -0,0 +1,15 @@
+def solution(heights):
+    answer = []
+    heights_length = len(heights)
+    for i in range(1, heights_length):
+        send = heights[-i]
+        for j in range(i+1, heights_length+1):
+            if heights[-j] > send:
+                answer.append(heights_length + j + 1)
+                break
+        else:
+            answer.append(0)
+            pass
+    answer.append(0)
+    answer.reverse()
+    return answer
