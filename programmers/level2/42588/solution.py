7dccbbadfd67f50d85926f213c6bd8e7e72aa224
Hyeongwoo Kim <snake1754@gmail.com>
2019-03-09 01:01:08 +0900

    Reformat pg-42588

--- programmers/level2/42588/solution.py
+++ programmers/level2/42588/solution.py
@@ -3,13 +3,13 @@ def solution(heights):
     heights_length = len(heights)
     for i in range(1, heights_length):
         send = heights[-i]
-        for j in range(i+1, heights_length+1):
+        for j in range(i + 1, heights_length + 1):
             if heights[-j] > send:
                 answer.append(heights_length - j + 1)
                 break
         else:
             answer.append(0)
-            
+
     answer.append(0)
     answer.reverse()
     return answer
