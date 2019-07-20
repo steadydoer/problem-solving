37cfeb86de1a3531484087b9f6b17d8a875ffc12
Hyeongwoo Kim <snake1754@gmail.com>
2019-07-20 17:14:40 +0900

    Solve pg-42585 using python

---
+++ programmers/level2/42585/solution.py
@@ -0,0 +1,23 @@
+# Programmers Coding Test Practice
+# Level 2
+#
+#     https://programmers.co.kr/learn/courses/30/lessons/42585
+#
+# ==============================================================================
+
+
+def solution(arrangement):
+    answer = 0
+    stack = [arrangement[0]]
+    for idx in range(1, len(arrangement)):
+        before_sign = arrangement[idx-1]
+        sign = arrangement[idx]
+        if sign == '(':
+            stack.append(sign)
+        else:
+            if before_sign == '(':
+                answer += len(stack) - 1
+            else:
+                answer += 1
+            stack.pop()
+    return answer
